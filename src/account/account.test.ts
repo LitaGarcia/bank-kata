0916426ecc4572a68d650a3cfc2cab96bbf3d36a
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-03 13:33:30 +0100

    REFACTOR: Refactor deposit case test

--- src/account/account.test.ts
+++ src/account/account.test.ts
@@ -1,18 +1,19 @@
 import {Account} from "./account.ts";
 
 describe('account', () => {
-    it('should deposit an amount in a balance of the account', () => {
-    //given
-    const accountNumber: number = 12317
-    const balance: number = 1000;
-    const amount: number = 100;
+    it('should deposit an amount in the balance of the account', () => {
+        //given
+        const accountNumber: number = 12317
+        const balance: number = 1000;
+        const amount: number = 100;
 
-    //when
-    const account: Account = new Account(accountNumber, balance);
-    account.deposit(100)
+        //when
+        const account: Account = new Account(accountNumber, balance);
+        const deposit: void = account.deposit(amount)
+        const getBalance: number = account._getBalance()
 
     //then
-    expect(account.balance).toEqual(1100)
+    expect(getBalance).toEqual(1100)
     })
     it('should get the balance of the account', () => {
     //given
