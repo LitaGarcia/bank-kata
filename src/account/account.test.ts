8d4505b3f23ea28a7f1e851b2fcbc3ab8e7a9a5b
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-03 17:07:33 +0100

    TEST: Create test to cover printStatment

--- src/account/account.test.ts
+++ src/account/account.test.ts
@@ -41,5 +41,23 @@ describe('account', () => {
         //then
         expect(currentBalance).toEqual(900)
     })
+    it('should printStatement from the account', () => {
+        //given
+        const mockClock: Clock = {
+            now: jest.fn(() => 12345)
+        };
+        const accountNumber: number = 12317;
+        const balance: number = 1000;
+        const account: Account = new Account(accountNumber, balance, mockClock);
+        const currentBalance: number = account._getBalance();
+
+        //when
+        const printStatement: Statement = account.printStatement();
+
+        //then
+        expect(printStatement).toEqual({
+            date: 12345,
+            balance: currentBalance
+        })
     }
 )
