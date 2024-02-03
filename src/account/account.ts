4843f9187e401a494ff73445974f0544545f73c6
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-03 17:12:31 +0100

    FEAT: Create printStatement method and clock

--- src/account/account.ts
+++ src/account/account.ts
@@ -1,10 +1,12 @@
 export class Account {
     private _balance: number;
     accountNumber: number;
+    private _clock: Clock;
 
-    constructor(accountNumber: number, balance: number) {
+    constructor(accountNumber: number, balance: number, clock: Clock) {
         this._balance = balance;
         this.accountNumber = accountNumber;
+        this._clock = clock;
     }
 
     _getBalance(): number {
@@ -18,4 +20,20 @@ export class Account {
     withdraw(amount: number): void {
         this._balance = this._balance - amount;
     }
+
+    printStatement(): Statement {
+        return {
+            date: this._clock.now(),
+            balance: this._getBalance()
+        }
+    }
+}
+
+export type Statement = {
+    date: number,
+    balance: number
+}
+
+export interface Clock {
+    now: () => number
 }
