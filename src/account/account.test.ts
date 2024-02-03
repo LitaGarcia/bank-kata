d0dcd6623d4952f17387c15a6f440d8be753801d
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-03 13:27:33 +0100

    TEST: Cover getBalance method

--- src/account/account.test.ts
+++ src/account/account.test.ts
@@ -14,5 +14,17 @@ describe('account', () => {
     //then
     expect(account.balance).toEqual(1100)
     })
+    it('should get the balance of the account', () => {
+    //given
+        const accountNumber: number = 12317
+        const balance: number = 1000;
+
+        //when
+        const account: Account = new Account(accountNumber, balance);
+        const getBalance: number = account._getBalance()
+
+        //then
+        expect(getBalance).toEqual(1000)
+    })
     }
 )
