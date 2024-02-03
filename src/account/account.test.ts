86d2b283f5a0b0279015a335fee7fb6c840900c9
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-03 22:16:12 +0100

    TEST: Create test to add an amount withdraw in the statementRepository

--- src/account/account.test.ts
+++ src/account/account.test.ts
@@ -78,4 +78,30 @@ describe('account', () => {
         //then
         expect(spyAccountStatementRepository).toHaveBeenCalledWith(statement)
     })
+    it('should add an statement with the amount withdraw in the repositoryStatement list', () => {
+        //given
+        const statement: Statement = {
+            date: 12345,
+            balance: 900,
+            amount: -100
+        };
+        const mockClock: Clock = {
+            now: jest.fn(() => 12345)
+        };
+        const mockAccountStatementRepository: StatementRepository = {
+            findAll: jest.fn(() => []),
+            save: jest.fn()
+        }
+        const spyAccountStatementRepository = jest.spyOn(mockAccountStatementRepository, 'save')
+        const accountNumber: number = 12317
+        const balance: number = 1000;
+        const amount: number = 100;
+        const account: Account = new Account(accountNumber, balance, mockClock, mockAccountStatementRepository);
+
+        //when
+        account.withdraw(amount)
+
+        //then
+        expect(spyAccountStatementRepository).toHaveBeenCalledWith(statement)
+    })
 })
