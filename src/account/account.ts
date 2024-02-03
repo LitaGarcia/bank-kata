7bf1bb756bf1facd0144b761137e9210dc3c13be
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-03 13:48:40 +0100

    FEAT: Create withdraw method

--- src/account/account.ts
+++ src/account/account.ts
@@ -1,16 +1,21 @@
 export class Account {
     private _balance: number;
     accountNumber: number;
-constructor(accountNumber: number, balance: number) {
-    this._balance = balance;
-    this.accountNumber = accountNumber;
-}
 
-_getBalance(): number {
-    return this._balance
-}
+    constructor(accountNumber: number, balance: number) {
+        this._balance = balance;
+        this.accountNumber = accountNumber;
+    }
 
-deposit(amount: number): void {
-    this._balance = amount + this._balance;
-}
+    _getBalance(): number {
+        return this._balance
+    }
+
+    deposit(amount: number): void {
+        this._balance = amount + this._balance;
+    }
+
+    withdraw(amount: number): void {
+        this._balance = this._balance - amount;
+    }
 }
