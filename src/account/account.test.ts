61365fdca542c46210116a8e3e7b43a4756ac226
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-04 18:32:12 +0100

    REFACTOR: Refactor account test suite

--- src/account/account.test.ts
+++ src/account/account.test.ts
@@ -1,6 +1,32 @@
 import {Account, Clock, Statement, StatementRepository} from "./account.ts";
+import {beforeEach} from "@jest/globals";
+import mock = jest.mock;
 
 describe('account', () => {
+    let mockClock: Clock;
+    let mockAccountStatementRepository: StatementRepository;
+    const accountNumber: number = 12317;
+    const balance: number = 1000;
+    let account: Account;
+    const mockFindAll = jest.fn((): Statement[] => []);
+    const mockSave = jest.fn();
+    const mockNow = jest.fn((): number => 12345)
+
+    beforeEach(() => {
+        mockClock = {
+            now: mockNow
+        };
+        mockAccountStatementRepository = {
+            findAll: mockFindAll,
+            save: mockSave
+        };
+        account = new Account(accountNumber, balance, mockClock, mockAccountStatementRepository);
+    });
+
+    afterEach(()=> {
+        jest.clearAllMocks();
+    })
+
     it('should printStatement list from the account', () => {
         //given
         const statementList: Statement[] = [{
@@ -14,17 +40,7 @@ describe('account', () => {
                 amount: -100
             }
         ];
-        const mockClock: Clock = {
-            now: jest.fn(() => 12345)
-        };
-        const mockAccountStatementRepository: StatementRepository = {
-            findAll: jest.fn(() => statementList),
-            save: jest.fn()
-        }
-        const spyAccountStatementRepository = jest.spyOn(mockAccountStatementRepository, 'findAll');
-        const accountNumber: number = 12317;
-        const balance: number = 1000;
-        const account: Account = new Account(accountNumber, balance, mockClock, mockAccountStatementRepository);
+        jest.spyOn(mockAccountStatementRepository, 'findAll').mockReturnValue(statementList);
 
         //when
         const printStatement: Statement[] = account.printStatement();
@@ -34,54 +50,34 @@ describe('account', () => {
     })
     it('should add an statement with the amount deposited in the repositoryStatement list', () => {
         //given
-        const statement: Statement = {
-            date: 12345,
-            balance: 1100,
-            amount: 100
-        };
-        const mockClock: Clock = {
-            now: jest.fn(() => 12345)
-        };
-        const mockAccountStatementRepository: StatementRepository = {
-            findAll: jest.fn(() => []),
-            save: jest.fn()
-        }
         const spyAccountStatementRepository = jest.spyOn(mockAccountStatementRepository, 'save')
-        const accountNumber: number = 12317;
-        const balance: number = 1000;
         const amount: number = 100;
-        const account: Account = new Account(accountNumber, balance, mockClock, mockAccountStatementRepository);
 
         //when
         account.deposit(amount);
 
         //then
+        const statement: Statement = {
+            date: 12345,
+            balance: 1100,
+            amount: 100
+        };
         expect(spyAccountStatementRepository).toHaveBeenCalledWith(statement)
     })
     it('should add an statement with the amount withdraw in the repositoryStatement list', () => {
         //given
-        const statement: Statement = {
-            date: 12345,
-            balance: 900,
-            amount: -100
-        };
-        const mockClock: Clock = {
-            now: jest.fn(() => 12345)
-        };
-        const mockAccountStatementRepository: StatementRepository = {
-            findAll: jest.fn(() => []),
-            save: jest.fn()
-        }
         const spyAccountStatementRepository = jest.spyOn(mockAccountStatementRepository, 'save')
-        const accountNumber: number = 12317
-        const balance: number = 1000;
         const amount: number = 100;
-        const account: Account = new Account(accountNumber, balance, mockClock, mockAccountStatementRepository);
 
         //when
         account.withdraw(amount)
 
         //then
+        const statement: Statement = {
+            date: 12345,
+            balance: 900,
+            amount: -100
+        };
         expect(spyAccountStatementRepository).toHaveBeenCalledWith(statement)
     })
 })
