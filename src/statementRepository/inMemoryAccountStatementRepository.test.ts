44f88ff6616b1c7cf4a1c711753559470d8e1d28
Lita Garcia <manuelagarciayebra@gmail.com>
2024-02-04 19:43:37 +0100

    TEST: Create new test to cover findAll and result methods

--- src/statementRepository/inMemoryAccountStatementRepository.test.ts
+++ src/statementRepository/inMemoryAccountStatementRepository.test.ts
@@ -13,5 +13,22 @@ describe('inMemoryAccountStatementRepository', () => {
     const result: [] = [];
     expect(findAll).toEqual(result)
     })
+    it('should save an statement correctly and actualize the statementList', () => {
+    //given
+    const statement: Statement = {
+        date: 12345,
+        balance: 1100,
+        amount: 100
+    };
+    const inMemoryAccountStatementRepository: StatementRepository = new InMemoryAccountStatementRepository();
+
+    //when
+    inMemoryAccountStatementRepository.save(statement);
+    const findAll: Statement[] = inMemoryAccountStatementRepository.findAll();
+
+    //then
+    const result: Statement[] = [statement];
+    expect(findAll).toEqual(result);
+    })
     }
 )
